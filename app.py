4d38bcf969380f950c28d425c4ef9dbbf231f4c6
Asaf Salant <63898696+AsafSalant@users.noreply.github.com>
2023-10-29 18:24:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ import newspaper
 from newspaper import Article
 app = Flask(__name__)
 
+
+
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    urls = ['https://www.theiwsr.com/key-trends-for-wine-in-2023-and-beyond/','https://8wines.com/blog/wine-trends-everything-you-need-to-know','https://www.wineandmore.com/stories/global-wine-trends/']
+    texts = []
+    for url in urls:
+        article = Article(url)
+        article.download()
+        article.parse()
+        texts.append(article.text)
+    
+    return texts
